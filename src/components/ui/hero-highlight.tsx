ec1a241757718a5c237937ffe7780c6d2c394d5e
AcevedoEsteban <astevenaa@gmail.com>
2024-05-24 01:02:15 -0700

    [frontend]style:landing page

--- src/components/ui/hero-highlight.tsx
+++ src/components/ui/hero-highlight.tsx
@@ -1,5 +1,7 @@
 'use client';
-import { cn } from '@/lib/utils';
+
+import { cn } from '../../lib/utils';
+
 import { useMotionValue, motion, useMotionTemplate } from 'framer-motion';
 import React from 'react';
 
@@ -29,7 +31,7 @@ export const HeroHighlight = ({
   return (
     <div
       className={cn(
-        'relative h-[40rem] flex items-center bg-white dark:bg-black justify-center w-full group',
+        'relative h-screen flex items-center bg-white dark:bg-black justify-center w-full group',
         containerClassName,
       )}
       onMouseMove={handleMouseMove}
